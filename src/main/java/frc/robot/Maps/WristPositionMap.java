2c9840bbfed3ddb0937ed6d8db1332925e78a3c3
CaIeb <calebdistelrobots@gmail.com>
2020-03-10 14:45:07 -0700

    Formatting

--- src/main/java/frc/robot/Maps/WristPositionMap.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.Maps;
-public class WristPositionMap {
- public static double score = 90;
- public static double home = 180;
- public static double down = 270;
-}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,40 +6,6 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-<<<<<<< Updated upstream
-import frc.robot.Subsystems.*;
-import frc.robot.Subsystems.WristSubsystem.*;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import frc.robot.Autonomous.*;
-import frc.robot.Sensors.*;
-public class RobotContainer {
- public static void RobotPeriodic() {
-    Smart_Dashboard.Smart_Dashboard_Periodic();
- }
- public static void RobotInit() {
-    Smart_Dashboard.Smart_Dashboard_RobotInit();
-    DriveSubsystem.driveInit();
- }
- public static void TeleOpInit() {
-
- }
- public static void TeleOp(){
-    Smart_Dashboard.Smart_Dashboard_TeleOp();
-    DriveSubsystem.driveTeleOp();
-    Wrist.WristTeleOp();
-    Intake.IntakeTeleOp();
-    Limelight.Update_Limelight_Tracking();
-    Limelight.Limelight_Tracking();
- }
- public static void AutoInit() {
-    Smart_Dashboard.Smart_Dashboard_AutoInit();
-    Autonomous.Auto();
- }
- public static void Auto(){
-    Scheduler.getInstance().run();
- }
-}
-=======
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
@@ -154,4 +120,3 @@ public class RobotContainer {
     return m_autochooser.getSelected();
   }
 }
->>>>>>> Stashed changes
